cbda6e4456f05cd5bb1fc38cb0279d2dd45bed5f
Patrik Sjöberg <voxar@spotify.com>
2013-09-17 22:24:29 +0200

    simplify example.rb
    Change-Id: Ib639f2bc3134fc202fdb8ea9693b31e3eb24bbe8

--- example.rb
+++ example.rb
@@ -1,30 +1,21 @@
 require './simulator'
 require './nuremote'
 
+app_path = "SomeAwesome.app"
+app_path = File.expand_path("~/Documents/Spotify/Code/iphoneRelease/DerivedData/Spotify+features/Build/Products/Debug-iphonesimulator/Spotify.app")
+sim = Simulator.new
+sim_arguments = [:iphone, :tall, :retina, :sdk => "6.1"]
+sim.start_app app_path, *sim_arguments
 
-def startNu
-  NuRemote::BlockingClient.new('localhost')
-end
-
-def startSimulator
-  arguments = [:iphone, :tall, :retina, :sdk => "6.1"]
-  
-  app_path = "SomeAwesome.app"
-  app_path = File.expand_path("~/Documents/Spotify/Code/iphoneRelease/DerivedData/Spotify+features/Build/Products/Debug-iphonesimulator/Spotify.app")
-  sim = Simulator.new
-  sim.start_app app_path, *arguments
-  sim
-end
-
-
-sim = startSimulator
 # Wait a while for simulator, and app, to start
 sleep 15
 # Connecto to the nu remote
-nu = startNu
+nu = NuRemote::BlockingClient.new('localhost')
 # Set application in required state
 nu.nu("(((UIApplication sharedApplication) delegate) getReadyForScreenshotNumber:1)")
 # Save a screenshot
 sim.save_screenshot "./screenshot.png"
+# Stop the nu client
+nu.close
 # Close the simulator
 sim.terminate
